ac9c7cf2dde5a3212c41ac85dbbfe11b2ad93a89
linuscys <linus.cys@gmail.com>
2014-10-16 15:56:46 +0800

    first commit

---
+++ XuiteSDK/Library/XuiteSDK.h
@@ -0,0 +1,18 @@
+//
+//  XuiteSDK.h
+//  XuiteSDK
+//
+//  Created by Chen Yang-Shen on 2014/10/14.
+//  Copyright (c) 2014年 Xuite. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef _XUITESDK_
+    #define _XUITESDK_
+
+    #import "XUSession.h"
+    #import "XUAuth.h"
+    #import "XUUser.h"
+    #import "XUApiManager.h"
+#endif
